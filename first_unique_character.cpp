4fe6503718d72713962739fa0f6b1192dec62c2e
Sunny Raj <94435695+rajnsunny@users.noreply.github.com>
2023-05-27 18:13:20 +0530

    Create first_unique_character.cpp
    Hashing Technique

---
+++ first_unique_character.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> ump;
+        for(auto a: s){
+            ump[a]++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(ump[s[i]] == 1) return i;
+        }
+        return -1;
+    }
+};
