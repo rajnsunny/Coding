dfa008913c37a56590ed4ad3fae51fe38c06eed2
Sunny Raj <94435695+rajnsunny@users.noreply.github.com>
2023-06-14 08:25:41 +0530

    Update Longest_Consecutive_Sequence.cpp

--- Longest_Consecutive_Sequence.cpp
+++ Longest_Consecutive_Sequence.cpp
@@ -22,4 +22,32 @@ public:
 
 // Optimal One
 
+int longestConsecutive(vector<int> &nums)
+{
+    int longest_cons_chain = 0;
+    unordered_map<int, bool> present;
+    unordered_map<int, bool> checked;
+    for (auto a : nums)
+    {
+        present[a] = true;
+    }
+    for (int i = 0; i < nums.size(); i++)
+    {
+        // !checked[nums[i]] means that number should not be part of a conseq sequence made already
+        if (!checked[nums[i]] && !present[nums[i] - 1])
+        {
+            int current_chain = 0;
+            int start = nums[i];
+            while (present[start])
+            {
+                current_chain++;
+                checked[start] = true;
+                start++;
+            }
+            longest_cons_chain = max(longest_cons_chain, current_chain);
+        }
+    }
+    return longest_cons_chain;
+}
+
 
